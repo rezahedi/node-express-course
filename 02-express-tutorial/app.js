aa7d5831f5d65b9fe18faf44536c71da8fca2982
Reza Zahedi <r.zahedi@gmail.com>
2024-11-20 22:37:29 -0800

    Refactor express port listener

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -5,14 +5,17 @@ const app = express();
 /**
  * Create HTTP server
  */
-const http = require('http')
+// const http = require('http')
 const port = 3000;
-app.set('port', port)
-const server = http.createServer(app)
-server.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
-})
+// app.set('port', port)
+// const server = http.createServer(app)
+// server.listen(port, () => {
+//     console.log(`app listening at http://localhost:${port}`)
+// })
 
+app.listen(port, () => {
+    console.log(`Local sercer is listening on port ${port}`)
+})
 
 app.use(express.static("./public"))
 
