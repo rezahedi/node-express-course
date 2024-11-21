5f592ea97d8cdaea0b55a10a98808ea49a8b3d37
Reza Zahedi <r.zahedi@gmail.com>
2024-11-20 21:10:10 -0800

    Start my api server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,23 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+
+
+/**
+ * Create HTTP server
+ */
+const http = require('http')
+const port = 3000;
+app.set('port', port)
+const server = http.createServer(app)
+server.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+})
+
+
+app.use(express.static("./public"))
+
+app.get('/api/xendpoint', (request, response) => {
+    return response.json({
+        message: 'My First API Endpoint!'
+    })
+})
