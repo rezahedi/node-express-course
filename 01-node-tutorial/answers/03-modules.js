0cca1a704a4f99c82ecd154d6a7141557a46bb3e
Reza Zahedi <r.zahedi@gmail.com>
2024-12-05 17:00:12 -0800

    06 alternative flavor

--- 01-node-tutorial/answers/03-modules.js
+++ 01-node-tutorial/answers/03-modules.js
@@ -1,5 +1,9 @@
 const names = require('./04-names.js')
 const greeting = require('./05-utils.js')
+const {items, singlePerson} = require('./06-alternative-flavor.js')
+
 greeting(names.firstPerson)
 greeting(names.secondPerson)
 greeting(names.thirdPerson)
+
+console.log(items, singlePerson)
