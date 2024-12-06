3e9234c7d9dbb592e2630fb0fe324f8a35c27958
Reza Zahedi <r.zahedi@gmail.com>
2024-12-05 17:05:54 -0800

    07 mind grenade

--- 01-node-tutorial/answers/03-modules.js
+++ 01-node-tutorial/answers/03-modules.js
@@ -1,6 +1,7 @@
 const names = require('./04-names.js')
 const greeting = require('./05-utils.js')
 const {items, singlePerson} = require('./06-alternative-flavor.js')
+require('./07-mind-grenade.js')
 
 greeting(names.firstPerson)
 greeting(names.secondPerson)
---
+++ 01-node-tutorial/answers/07-mind-grenade.js
@@ -0,0 +1,8 @@
+const firstValue = 3;
+const secondValue = 4;
+
+const sumValues = (a, b) => {
+  console.log(`Sum: ${a} + ${b} = ${a+b}`)
+}
+
+sumValues(firstValue, secondValue)
