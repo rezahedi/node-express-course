756ea1e0640290f1561434eba3f77c2a463b3441
Reza Zahedi <r.zahedi@gmail.com>
2024-12-06 10:13:52 -0800

    Guess game but have issue

--- 01-node-tutorial/answers/prompter.js
+++ 01-node-tutorial/answers/prompter.js
@@ -22,17 +22,39 @@ const getBody = (req, callback) => {
 
 // here, you could declare one or more variables to store what comes back from the form.
 let item = "Enter something below.";
+const RANDOM_NUMBER = Math.floor(Math.random() * 100);
+const GUESS_LIMIT = 10;
+let guesses = 0;
+
+const guessChecker = (guessedNumber) => {
+  console.log("guesschecker run")
+  guesses++;
+
+  if (guessedNumber === RANDOM_NUMBER) {
+    return `You found it with ${guesses} guesses!`;
+  } else if (guessedNumber < RANDOM_NUMBER) {
+    return `You are too low. Guess again!`;
+  } else {
+    return `You are too high. Guess again!`;
+  }
+};
 
 // here, you can change the form below to modify the input fields and what is displayed.
 // This is just ordinary html with string interpolation.
 const form = () => {
+  const result = parseInt(item) ? guessChecker( parseInt(item) ) : "";
   return `
   <body>
-  <p>${item}</p>
   <form method="POST">
-  <input name="item"></input>
-  <button type="submit">Submit</button>
+  <h2>Number Guessing Game: ${guesses}/${GUESS_LIMIT}</h2>
+  ${RANDOM_NUMBER} - ${item}
+  <p>You have ten guesses to find the number I'm thinking of.</p>
+  <p>By entering a number between 1 and 100, you can guess what number I'm thinking of.</p>
+  <p>I'm giving you clue by telling you if you're is lower or higher than mine.</p>
+  Your number: <input name="guessed_number" type="number" min="1" max="100" style="padding:5px;text-align:center" />
+  <button type="submit" style="padding:5px">Guess</button>
   </form>
+  ${result}
   </body>
   `;
 };
@@ -44,8 +66,8 @@ const server = http.createServer((req, res) => {
     getBody(req, (body) => {
       console.log("The body of the post is ", body);
       // here, you can add your own logic
-      if (body["item"]) {
-        item = body["item"];
+      if (body["guessed_number"]) {
+        item = body["guessed_number"];
       } else {
         item = "Nothing was entered.";
       }
