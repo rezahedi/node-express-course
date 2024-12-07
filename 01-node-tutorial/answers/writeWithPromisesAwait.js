3a56d4f1ca070a15ddb04678da7d106e0522cf71
Reza Zahedi <r.zahedi@gmail.com>
2024-12-06 22:29:11 -0800

    Write in file with await

---
+++ 01-node-tutorial/answers/writeWithPromisesAwait.js
@@ -0,0 +1,14 @@
+const { writeFile, readFile } = require('fs').promises;
+
+const writer = async () => {
+  const fileName = './temporary/temp.txt';
+
+  await writeFile(fileName, "01 First line.\n");
+  await writeFile(fileName, "02 Second line.\n", {flag: 'a'});
+  await writeFile(fileName, "03 Third line.\n", {flag: 'a'});
+
+  const fileContent = await readFile(fileName, 'utf8')
+  console.log(fileContent)
+}
+
+writer()
