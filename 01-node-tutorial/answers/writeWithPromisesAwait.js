8284ae47cea75344b4b53c838f78e16099deed3b
Reza Zahedi <r.zahedi@gmail.com>
2024-12-06 22:35:03 -0800

    Read Write in file using async

--- 01-node-tutorial/answers/writeWithPromisesAwait.js
+++ 01-node-tutorial/answers/writeWithPromisesAwait.js
@@ -6,9 +6,18 @@ const writer = async () => {
   await writeFile(fileName, "01 First line.\n");
   await writeFile(fileName, "02 Second line.\n", {flag: 'a'});
   await writeFile(fileName, "03 Third line.\n", {flag: 'a'});
+}
 
+const reader = async () => {
+  const fileName = './temporary/temp.txt';
+  
   const fileContent = await readFile(fileName, 'utf8')
   console.log(fileContent)
 }
 
-writer()
+const readWrite = async () => {
+  await writer();
+  await reader();
+}
+
+readWrite()
