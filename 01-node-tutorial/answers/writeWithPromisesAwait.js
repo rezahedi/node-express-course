fd50fef2e13e82c5e2bdab8ee11ab4060161529d
Reza Zahedi <r.zahedi@gmail.com>
2024-12-06 22:37:22 -0800

    Use const for filemane

--- 01-node-tutorial/answers/writeWithPromisesAwait.js
+++ 01-node-tutorial/answers/writeWithPromisesAwait.js
@@ -1,17 +1,14 @@
 const { writeFile, readFile } = require('fs').promises;
+const FILE_NAME = './temporary/output.txt';
 
 const writer = async () => {
-  const fileName = './temporary/temp.txt';
-
-  await writeFile(fileName, "01 First line.\n");
-  await writeFile(fileName, "02 Second line.\n", {flag: 'a'});
-  await writeFile(fileName, "03 Third line.\n", {flag: 'a'});
+  await writeFile(FILE_NAME, "01 First line.\n");
+  await writeFile(FILE_NAME, "02 Second line.\n", {flag: 'a'});
+  await writeFile(FILE_NAME, "03 Third line.\n", {flag: 'a'});
 }
 
 const reader = async () => {
-  const fileName = './temporary/temp.txt';
-  
-  const fileContent = await readFile(fileName, 'utf8')
+  const fileContent = await readFile(FILE_NAME, 'utf8')
   console.log(fileContent)
 }
 
