8c73bcaf5438f3f55f8a23036e848c87f9de4d7e
Reza Zahedi <r.zahedi@gmail.com>
2024-12-09 09:28:08 -0800

    Add time log to events

--- 01-node-tutorial/answers/customEmitter.js
+++ 01-node-tutorial/answers/customEmitter.js
@@ -1,10 +1,10 @@
 const EventEmitter = require('events');
 const fileLogEmitter = new EventEmitter();
 
-fileLogEmitter.on("created", (msg, fileName, userName) => console.log(msg, fileName, userName));
-fileLogEmitter.on("published", (msg, fileName, userName) => console.log(msg, fileName, userName));
-fileLogEmitter.on("moved", (msg, fileName, userName) => console.log(msg, fileName, userName));
-fileLogEmitter.on("removed", (msg, fileName, userName) => console.log(msg, fileName, userName));
+fileLogEmitter.on("created", (msg, fileName, userName) => console.log(new Date().toTimeString(), msg, fileName, userName));
+fileLogEmitter.on("published", (msg, fileName, userName) => console.log(new Date().toTimeString(), msg, fileName, userName));
+fileLogEmitter.on("moved", (msg, fileName, userName) => console.log(new Date().toTimeString(), msg, fileName, userName));
+fileLogEmitter.on("removed", (msg, fileName, userName) => console.log(new Date().toTimeString(), msg, fileName, userName));
 
 // emit events simulation
 fileLogEmitter.emit("created", "File created", "file.txt", "Reza");
