160a2a5b537e10ac6fae4e1ae61e3002b89539a4
Reza Zahedi <r.zahedi@gmail.com>
2024-12-09 09:27:12 -0800

    Add fakewitetime to emit event

--- 01-node-tutorial/answers/customEmitter.js
+++ 01-node-tutorial/answers/customEmitter.js
@@ -11,3 +11,21 @@ fileLogEmitter.emit("created", "File created", "file.txt", "Reza");
 fileLogEmitter.emit("published", "File published", "john-data.txt", "John");
 fileLogEmitter.emit("moved", "File moved", "financial_records.txt", "Sarah");
 fileLogEmitter.emit("removed", "File removed", "emit-test.js", "David");
+
+
+const fakeWaitTimerForEvent = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 1000)
+  })
+}
+const doWait = async () => {
+  await fakeWaitTimerForEvent()
+  fileLogEmitter.emit("published", "File published", "john-data.txt", "John");
+  await fakeWaitTimerForEvent()
+  fileLogEmitter.emit("created", "File created", "file.txt", "Reza");
+  await fakeWaitTimerForEvent()
+  fileLogEmitter.emit("removed", "File removed", "emit-test.js", "David");
+}
+doWait()
