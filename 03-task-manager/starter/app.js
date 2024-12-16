85a535486c665b94457776d0fe0d462859b8d9c3
Reza Zahedi <r.zahedi@gmail.com>
2024-12-15 17:48:35 -0800

    Initiate first route

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
+const port = 3000;
+
+app.listen(port, console.log(`Local server is listening on port ${port}`))
