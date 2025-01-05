ed7cfb7658986b832a65d8158c5df4084c258e51
Reza Zahedi <r.zahedi@gmail.com>
2025-01-04 23:11:20 -0800

    Create the first token

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,6 +1,25 @@
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
+
 const login = async (req, res) => {
   const { username, password } = req.body
-  res.send(`Fake login route: username: ${username}, password: ${password}`)
+  if (!username || !password) {
+    throw new CustomAPIError('Username or password is missing!', 400)
+  }
+
+  // Create a fake login
+  const id = new Date().getTime()
+
+  const token = jwt.sign(
+    {
+      id,
+      username,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: '30d' },
+  )
+
+  res.status(200).json({message: 'User created', token})
 }
 
 const dashboard = async (req, res) => {
