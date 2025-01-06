84fa2386e2b5cf6cadd4618c73ae9a08902ea4cc
Reza Zahedi <r.zahedi@gmail.com>
2025-01-06 11:46:44 -0800

    Create my frontend route

---
+++ 05-JWT-Basics/myGroupChatAPI/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express')
+
+const app = express()
+
+// Middlewares
+app.use(express.static('./public'))
+app.use(express.json())
+
+
+// Routes
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
